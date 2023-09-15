9af6144aae7a58753c6c417688f873cc882ecae6
Mads Hermansen <mads.d.hermansen+main@gmail.com>
2023-09-15 19:52:32 +0200

    intial changes to homepage

---
+++ vite-project/src/Home.tsx
@@ -0,0 +1,16 @@
+
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="Logo">
+                <img src="" alt="logo"/>
+            </div>
+            <div className="Search">
+                <input type="text" placeholder="Search" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
