2fd7aa784e1be2298b45f423fc02973bcc908269
Lucas Tran <lucastra@stud.ntnu.no>
2023-09-17 15:38:44 +0200

    Let user access the website without login

--- vite-project/src/script.ts
+++ vite-project/src/script.ts
@@ -1,52 +1,12 @@
 const clientId = "8e124b2906924f8a896a5d55d01c310b"; // Replace with your client id
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code"); 
- 
-export async function redirectToAuthCodeFlow(clientId: string) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
+const secretId = "31a8d64235f2429295252b7fdd3f1387"
 
-    localStorage.setItem("verifier", verifier);
-
+export async function getAccessToken(clientId: string): Promise<string> {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
-    params.append("response_type", "code");
+    params.append("client_secret", secretId)
+    params.append("grant_type", "client_credentials");
     params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("scope", "user-read-private user-read-email");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
-
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(length: number) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier: string) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
-
-export async function getAccessToken(clientId: string, code: string): Promise<string> {
-    const verifier = localStorage.getItem("verifier");
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
@@ -75,22 +35,19 @@ async function fetchAudioFeatures(token: string, id: string): Promise<any> {
     return await result.json();
 }
 
-if (!code) {
-    redirectToAuthCodeFlow(clientId);
+
+var accessToken:string
+if (sessionStorage.getItem("accesstoken") == null) {
+    accessToken = await getAccessToken(clientId);
+    sessionStorage.setItem("accesstoken", accessToken)
 } else {
-    var accessToken:string
-    if (sessionStorage.getItem("accesstoken") == null) {
-        accessToken = await getAccessToken(clientId, code);
-        sessionStorage.setItem("accesstoken", accessToken)
-    } else {
-        accessToken = String(sessionStorage.getItem("accesstoken"))
-    }
-
-    const songId = await fetchSongInfo(accessToken)
-    console.log(songId.tracks.items[0].id)
-
-    const audioFeatures = await fetchAudioFeatures(accessToken, songId.tracks.items[0].id)
-    console.log(audioFeatures)
-    const audioFeaturesDict = {danceability: audioFeatures.danceability, energy: audioFeatures.energy, loudness: audioFeatures.loudness, positivity: audioFeatures.valence}
-    console.log(audioFeaturesDict)
+    accessToken = String(sessionStorage.getItem("accesstoken"))
 }
+
+const songInfo = await fetchSongInfo(accessToken)
+console.log(songInfo.tracks.items[0].id)
+
+const audioFeatures = await fetchAudioFeatures(accessToken, songInfo.tracks.items[0].id)
+console.log(audioFeatures)
+const audioFeaturesDict = {danceability: audioFeatures.danceability, energy: audioFeatures.energy, loudness: audioFeatures.loudness, positivity: audioFeatures.valence}
+console.log(audioFeaturesDict)
