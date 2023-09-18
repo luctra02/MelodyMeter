c8775ab2d99aa41157c355601226294703eeab59
Lucas Tran <lucastra@stud.ntnu.no>
2023-09-18 07:06:02 +0200

    Add functionality for search and display artists

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,19 +3,33 @@ import Home from './components/Home'
 import Navbar from './components/navbar'
 import Favourite from './components/favourite'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import SongDisplay from './components/SongDisplay';
+import GreyBox from './components/GreyBox';
+import DisplaySearch from './components/DisplaySearch';
+import { useState } from "react"
+
+
+
 
 function App() {
 
-  return (
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function updateSearchTerm(searchTerm:string){
+    setSearchTerm(searchTerm);
+  }
   
+  return (
     <Router >
       <div className="App">
         <Navbar />
 
       </div>
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/' element={<Home updateSearchTerm ={updateSearchTerm}/>}/>
         <Route path='/favourite' element={<Favourite />}/>
+        <Route path='/songDisplay' element={<SongDisplay />}/>
+        <Route path='/searchDisplay' element={<DisplaySearch searchTerm={searchTerm} />}/>
       </Routes>
     </Router>
 
