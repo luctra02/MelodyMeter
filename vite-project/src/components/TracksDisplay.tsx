3cb14ab25b4df7ffea08d01e8060bb6907adbd5f
Mads Hermansen <mdherman@stud.ntnu.no>
2023-09-19 21:07:30 +0200

    styling on songs

--- vite-project/src/components/TracksDisplay.tsx
+++ vite-project/src/components/TracksDisplay.tsx
@@ -14,17 +14,12 @@ function TracksDisplay(){
     const [playlistTracksArray, setPlaylistTracksArray] = useState<any[]>([]);
     
 
-    function HandleClickAlbum(item: any){
+    function HandleClick(item: any){
         navigate('/SongDisplay', {state: {albumId: item.id}});
-    }
-
-    function HandleClickPlaylist(item: any){
-        navigate('/SongDisplay', {state: {playlistId: item.track.id}});
     }
     
 
     useEffect(() => {
-        console.log(albumId)
         const getStats = async () => {
             const sessionKey = sessionStorage.getItem("accesstoken");
             if(albumId){
@@ -49,22 +44,26 @@ function TracksDisplay(){
         <h1>Tracks</h1>
         <div className="Song-List">
             {albumTracksArray.map((item: any) => (
-                <button key={item.id} onClick={() => HandleClickAlbum(item)}>
-                    <div className="Song">
+                <button className="displayIndiviualSongsButton" key={item.id} onClick={() => HandleClick(item)}>
+                    <div className="Song displayIndiviualSongs">
                         {/* <img src={item.images.length > 0  ? item.images[0].url: "https://i.scdn.co/image/ab6761610000e5eba1b1a48354e9a91fef58f651"} className='Song-Image'></img> */}
-                        <h3>{item.name} - 
+                        <h3>{item.name}</h3>
+                        <h3 className="songSpecificInfo">
                         {item.artists.map((artist: any, index: number) => (
                                 index === item.artists.length - 1
                                 ? artist.name + ' '  // Don't add a comma after the last item
                                 : artist.name + ', ' // Add a comma after all other items
                             ))}  
-                        {Math.floor(item.duration_ms/1000/60) }:{Math.floor(item.duration_ms/1000 % 60) < 10 ? "0" + Math.floor(item.duration_ms/1000 % 60):Math.floor(item.duration_ms/1000 % 60)}</h3>
+                        </h3>
+                        <h3 className="songSpecificInfo">
+                        {Math.floor(item.duration_ms/1000/60) }:{Math.floor(item.duration_ms/1000 % 60) < 10 ? "0" + Math.floor(item.duration_ms/1000 % 60):Math.floor(item.duration_ms/1000 % 60)}
+                        </h3> 
                     </div>
                 </button>))}
         </div>
         <div className="Song-List">
             {playlistTracksArray.map((item: any,) => (
-                <button className="displayIndiviualSongsButton"key={item.track.id} onClick={() => HandleClick(item)}>
+                <button className="displayIndiviualSongsButton" key={item.track.id} onClick={() => HandleClick(item)}>
                     <div className="Song displayIndiviualSongs">
                         {/* <img src={item.images.length > 0  ? item.images[0].url: "https://i.scdn.co/image/ab6761610000e5eba1b1a48354e9a91fef58f651"} className='Song-Image'></img> */}
                         <h3>
